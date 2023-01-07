a8a0ab6e44832e8b011e9e690c2d39b8a1c32fb7
Daguer <dgrfps@gmail.com>
2023-01-06 23:46:53 -0300

    Added movement smoothness

--- Player/CameraController.cs
+++ Player/CameraController.cs
@@ -1,36 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-// SCRIPT BY Brackeys LOCATED AT YOUTUBE: FIRST PERSON MOVEMENT in Unity - FPS Controller
-
-public class CameraController : MonoBehaviour
+public class mCameraController : MonoBehaviour
 {
-    [SerializeField] float sensivity_x = 1;
-    [SerializeField] float sensivity_y = 1;
-    
+    [System.Serializable]
+    struct Sensivity
+    {
+        public float x;
+        public float y;
+        public float scale;
+        public Sensivity(float x, float y, float s)
+        {
+            this.x = x; this.y = y; this.scale = s;
+        }
+    }
+
+    [SerializeField] Sensivity sensivity = new Sensivity(1, 1, 100);
+    [SerializeField] float smooth = .125f;
+    [SerializeField] float maxYaw = 80f;
     [SerializeField] Transform body;
+
     float rotation = 0;
-    
-    PlayerController p;
+    float s_x;
 
-    private void Start() {
-        p = body.GetComponent<PlayerController>();
 
+    private void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update() {
-        float x = Input.GetAxis("Mouse X") * sensivity_x * 100 * Time.deltaTime;
-        float y = Input.GetAxis("Mouse Y") * sensivity_y * 100 * Time.deltaTime;
-        
-        rotation -= y;
-        rotation = Mathf.Clamp(rotation, -80, 80);
+    private void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * sensivity.x * sensivity.scale * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensivity.y * sensivity.scale * Time.deltaTime;
 
-        if(p.playerState == PlayerController.PlayerState.Playing)
-        {
-            transform.localRotation = Quaternion.Euler(rotation, 0, 0);
-            body.Rotate(Vector3.up * x);
-        }
+        rotation -= y;
+        rotation = Mathf.Clamp(rotation, -Mathf.Abs(maxYaw), Mathf.Abs(maxYaw));
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotation, 0, 0), Time.deltaTime / smooth);
+        s_x = Mathf.Lerp(s_x, x, Time.deltaTime / smooth);
+        body.Rotate(body.transform.up * s_x);
     }
 }
