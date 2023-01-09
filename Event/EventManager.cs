36abc6c90616e9f8b4b6230641ac0e27925b96fe
Daguer <dgrfps@gmail.com>
2023-01-09 20:18:45 -0300

    possible make multiple events on a single ID

--- Event/EventManager.cs
+++ Event/EventManager.cs
@@ -15,6 +15,7 @@ public class GEvent
         this.action = evt;
         this.called_times = 0;
     }
+    
 
     public void Call(object[] data)
     {        
@@ -33,37 +34,51 @@ public class GEvent
     }
 }
 
-public class EventManager
+public class EventManager : MonoBehaviour
 {
-    static Dictionary<string, GEvent> events = new Dictionary<string, GEvent>();
+    static List<KeyValuePair<string, GEvent>> events = new List<KeyValuePair<string, GEvent>>();
     
     public static void RegisterEvent(GEvent e)
     {
-        if(!events.ContainsKey(e.GetID()))
-            events.Add(e.GetID(), e);
+        events.Add(new KeyValuePair<string, GEvent>(e.GetID(), e));
     }
 
-    public static void CallEvent(string EventID, params object[] data)
+    //Now it's possible to assing multiple callbacks to the same id!
+    public static void CallEvents(string EventID, params object[] data)
     {
-        GEvent e = null;
-        
-        if(events.TryGetValue(EventID, out e))
-            e.Call(data);
+        events.ForEach(e => { 
+            if(e.Key == EventID) e.Value.Call(data);
+        });
     }
 
-     public static void CallEventOnce(string EventID, params object[] data)
+    public static void CallEventsOnce(string EventID, params object[] data)
     {
-        GEvent e = null;
-        
-        if(events.TryGetValue(EventID, out e))
-            if(e.CalledTimes() == 0)
-                e.Call(data);
+        events.ForEach(e => { if(e.Key == EventID) if(e.Value.CalledTimes() == 0) e.Value.Call(data); });
     }
 
-    public static GEvent GetEvent(String EventID)
+    public static List<GEvent> GetEvents(String EventID)
     {
-        GEvent e = null;        
-        return events.TryGetValue(EventID, out e) ? e : null;
+        List<GEvent> _ev = new List<GEvent>();      
+
+        events.ForEach(e => {
+            if(e.Key == EventID) _ev.Add(e.Value);
+        });
+
+        return _ev;
+    }
+
+    static EventManager _inst;
+    void Awake()
+    {
+        if(_inst != null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }else
+        {
+            _inst = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
 }
